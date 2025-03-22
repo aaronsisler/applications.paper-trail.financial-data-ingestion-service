9a50e6ab9e992b0cfdc9262bdae0bb7316534cc9
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-03-21 22:12:19 -0400

    I like this more and seems more controllable

--- app/src/main/java/com/ebsolutions/papertrail/financialdataingestionservice/accounttransaction/AccountTransactionSubscriber.java
+++ app/src/main/java/com/ebsolutions/papertrail/financialdataingestionservice/accounttransaction/AccountTransactionSubscriber.java
@@ -1,27 +1,66 @@
 package com.ebsolutions.papertrail.financialdataingestionservice.accounttransaction;
 
-import io.awspring.cloud.sqs.annotation.SqsListener;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageBatchRequest;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageBatchRequestEntry;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
+@EnableScheduling
+@EnableAsync
 public class AccountTransactionSubscriber {
+  //  private final SqsAsyncClient sqsAsyncClient;
+  private final SqsClient sqsClient;
 
-  @SqsListener("${infrastructure.messaging.queue-url}")
-  public void receiveRecordMessage(String accountTransaction) {
-    log.info("Received message: {}", accountTransaction);
-    //    userRepository.save(new User(event.id(), event.username(), event.email()));
-  }
+  @Value("${infrastructure.messaging.queue-url:`Queue name not found in environment`}")
+  protected String queueUrl;
 
-  @SqsListener("${infrastructure.messaging.queue-url}")
-  public void receiveRecordMessages(List<String> accountTransactions) {
-    for (String accountTransaction : accountTransactions) {
-      log.info("Received message: {}", accountTransaction);
+  @Async
+  @Scheduled(fixedRate = 1000, initialDelay = 1000)
+  public void consumeMessages() {
+    ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
+        .queueUrl(queueUrl)
+        .maxNumberOfMessages(10)
+        .waitTimeSeconds(2)
+        .build();
+
+
+    List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
+
+    // Exit the loop if no messages on the queue
+    if (messages.isEmpty()) {
+      return;
     }
-    //    userRepository.save(new User(event.id(), event.username(), event.email()));
+
+    List<DeleteMessageBatchRequestEntry> deleteMessageBatchRequestEntries = new ArrayList<>();
+
+    for (Message message : messages) {
+      System.out.println("Message");
+      System.out.println("Thread: " + Thread.currentThread().threadId());
+      System.out.println(message.messageId());
+      System.out.println(message.body());
+      deleteMessageBatchRequestEntries.add(
+          DeleteMessageBatchRequestEntry.builder().id(message.messageId()).build());
+    }
+
+    DeleteMessageBatchRequest deleteMessageBatchRequest =
+        DeleteMessageBatchRequest.builder().queueUrl(queueUrl)
+            .entries(deleteMessageBatchRequestEntries)
+            .build();
+
+    sqsClient.deleteMessageBatch(deleteMessageBatchRequest);
   }
 }
