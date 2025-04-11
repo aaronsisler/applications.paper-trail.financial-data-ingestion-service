df016ff73faad1b1354e431b902582e05f1d7d4e
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-04-11 17:22:00 -0400

    Taking out the BaseTestConfig until it is needed

--- app/src/test/java/com/ebsolutions/papertrail/financialdataingestionservice/tooling/BaseTest.java
+++ app/src/test/java/com/ebsolutions/papertrail/financialdataingestionservice/tooling/BaseTest.java
@@ -4,7 +4,6 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -13,11 +12,4 @@ import org.springframework.test.web.servlet.MockMvc;
 public class BaseTest {
   @Autowired
   protected MockMvc mockMvc;
-
-  @TestConfiguration
-  /*
-   * Pulling this into BaseTest since there were issues when it was a separate file
-   */
-  public static class BaseTestConfiguration {
-  }
 }
