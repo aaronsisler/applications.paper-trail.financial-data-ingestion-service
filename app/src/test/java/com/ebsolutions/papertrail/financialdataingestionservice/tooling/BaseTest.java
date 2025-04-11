092cc32d779ab3df3f01df539fed520895dc54e6
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-04-11 17:24:42 -0400

    Adding back in BaseTestConfig since it is mad for a different reason

--- app/src/test/java/com/ebsolutions/papertrail/financialdataingestionservice/tooling/BaseTest.java
+++ app/src/test/java/com/ebsolutions/papertrail/financialdataingestionservice/tooling/BaseTest.java
@@ -4,6 +4,7 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -12,4 +13,11 @@ import org.springframework.test.web.servlet.MockMvc;
 public class BaseTest {
   @Autowired
   protected MockMvc mockMvc;
+
+  @TestConfiguration
+  /*
+   * Pulling this into BaseTest since there were issues when it was a separate file
+   */
+  public static class BaseTestConfiguration {
+  }
 }
