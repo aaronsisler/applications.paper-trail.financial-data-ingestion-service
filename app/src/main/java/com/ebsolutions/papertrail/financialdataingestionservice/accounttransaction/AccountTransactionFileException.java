f092873b42f7ca42d57334482a91ccb8c3fa5ebb
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-04-13 09:42:46 -0400

    Trying to reduce Sonar issues

--- app/src/main/java/com/ebsolutions/papertrail/financialdataingestionservice/accounttransaction/AccountTransactionFileException.java
+++ app/src/main/java/com/ebsolutions/papertrail/financialdataingestionservice/accounttransaction/AccountTransactionFileException.java
@@ -1,10 +1,18 @@
 package com.ebsolutions.papertrail.financialdataingestionservice.accounttransaction;
 
+import java.io.Serial;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class AccountTransactionFileException extends RuntimeException {
-  public List<ErrorMessageEnvelope> errorMessageEnvelopes = new ArrayList<>();
+  @Serial
+  private static final long serialVersionUID = 1;
+
+  private List<ErrorMessageEnvelope> errorMessageEnvelopes = new ArrayList<>();
 
   public AccountTransactionFileException(String message,
                                          List<ErrorMessageEnvelope> errorMessageEnvelopes) {
