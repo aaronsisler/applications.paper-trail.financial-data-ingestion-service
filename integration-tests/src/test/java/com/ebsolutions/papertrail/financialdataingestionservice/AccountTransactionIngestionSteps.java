5e50af6f87ed2b2fa01b09f1bd60724527a65972
Aaron Sisler <aaronsisler@users.noreply.github.com>
2025-04-20 19:20:16 -0400

    Feature/integration test account queue (#17)
    * Base test and file in place. Need to fill out the pieces next
    
    * Working up until the 202 response back from endpoint
    
    * Working integration test and updated the application up to be a little cleaner in the int test as well
    
    * Tweaking to use base step a little more
    
    * Final push of integration tests overall
    
    * MVB and CL
    
    * Removing a testing log

---
+++ integration-tests/src/test/java/com/ebsolutions/papertrail/financialdataingestionservice/AccountTransactionIngestionSteps.java
@@ -0,0 +1,123 @@
+package com.ebsolutions.papertrail.financialdataingestionservice;
+
+import com.ebsolutions.papertrail.financialdataingestionservice.config.TestConstants;
+import com.ebsolutions.papertrail.financialdataingestionservice.model.AccountTransaction;
+import com.ebsolutions.papertrail.financialdataingestionservice.model.AccountTransactionFileEnvelope;
+import com.ebsolutions.papertrail.financialdataingestionservice.model.SupportedInstitution;
+import com.ebsolutions.papertrail.financialdataingestionservice.tooling.BaseStep;
+import com.ebsolutions.papertrail.financialdataingestionservice.tooling.QueueMessageUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import software.amazon.awssdk.services.sqs.model.Message;
+
+public class AccountTransactionIngestionSteps extends BaseStep {
+  @Autowired
+  protected QueueMessageUtil queueMessageUtil = new QueueMessageUtil(endpoint, queueUrl);
+  private ResponseEntity<AccountTransactionFileEnvelope> response;
+  private MockMultipartFile mockMultipartFile;
+  private String accountId;
+  private String supportedInstitution;
+
+  @And("the account transaction envelope has a valid file with a valid account transaction")
+  public void theAccountTransactionEnvelopeHasAValidFileWithAValidAccountTransaction() {
+    mockMultipartFile =
+        new MockMultipartFile("file",
+            "test.txt",
+            MediaType.TEXT_PLAIN_VALUE,
+            "1450,Chipotle,2025-09-13".getBytes()
+        );
+  }
+
+  @And("the account id in the account transaction envelope is valid")
+  public void theAccountIdInTheAccountTransactionEnvelopeIsValid() {
+    accountId = "1";
+  }
+
+  @And("the supported institution in the account transaction envelope is valid")
+  public void theSupportedInstitutionInTheAccountTransactionEnvelopeIsValid() {
+    supportedInstitution = SupportedInstitution.AMEX.getValue();
+  }
+
+  @When("the ingest account transactions endpoint is invoked")
+  public void theIngestAccountTransactionsEndpointIsInvoked() throws IOException {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+    ByteArrayResource fileAsResource = new ByteArrayResource(mockMultipartFile.getBytes()) {
+      @Override
+      public String getFilename() {
+        return mockMultipartFile.getOriginalFilename();
+      }
+    };
+
+    MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+    body.add("accountId", accountId);
+    body.add("supportedInstitution", supportedInstitution);
+    body.add("file", fileAsResource);
+
+    HttpEntity<MultiValueMap<String, Object>> requestEntity
+        = new HttpEntity<>(body, headers);
+
+    response =
+        restTemplate.postForEntity(
+            TestConstants.BASE_URL + "/" + TestConstants.ACCOUNT_TRANSACTIONS_URI,
+            requestEntity,
+            AccountTransactionFileEnvelope.class);
+  }
+
+  @Then("the correct accepted response is returned from the ingest transactions endpoint")
+  public void theCorrectAcceptedResponseIsReturnedFromTheIngestTransactionsEndpoint() {
+    Assertions.assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+  }
+
+  @And("the account transactions are published to the queue with the correct account transaction")
+  public void theAccountTransactionsArePublishedToTheQueueWithTheCorrectAccountTransaction()
+      throws JsonProcessingException, InterruptedException {
+    List<Message> messages;
+    Instant pollingEnd =
+        Instant.now().plusMillis(TestConstants.QUEUE_POLLING_WAIT_PERIOD_IN_MILLISECONDS);
+
+    do {
+      // Wait between each polling since consumer is fast
+      Thread.sleep(100);
+      // Check if any messages
+      messages = queueMessageUtil.consume();
+      // If messages are found, break for the assertions
+      if (!messages.isEmpty()) {
+        break;
+      }
+    } while (!Instant.now().isAfter(pollingEnd));
+
+    // If not messages found after timeout, fail test
+    if (messages.isEmpty()) {
+      Assertions.fail("We didn't find any messages on the queue");
+    }
+
+    // Check the account transaction is correct
+    AccountTransaction accountTransaction =
+        objectMapper.readValue(messages.getFirst().body(), AccountTransaction.class);
+
+    // Check the account id on the account transaction is correct
+    Assertions.assertEquals(1450, accountTransaction.getAmount());
+    Assertions.assertEquals("Chipotle", accountTransaction.getDescription());
+    Assertions.assertEquals(LocalDate.of(2025, 9, 13), accountTransaction.getTransactionDate());
+    Assertions.assertEquals(Integer.parseInt(accountId), accountTransaction.getAccountId());
+  }
+}
--- integration-tests/src/test/resources/features/account_transaction_ingestion.feature
+++ integration-tests/src/test/resources/features/account_transaction_ingestion.feature
@@ -1,4 +1,10 @@
-Feature: Integration: Account Transaction
+Feature: Integration: Account Transaction Ingestion
 
-  Scenario: Create and Get All Account Transactions
+  Scenario: Ingest Account Transactions and Check Account Transaction Queue
     Given application is up
+    And the account transaction envelope has a valid file with a valid account transaction
+    And the account id in the account transaction envelope is valid
+    And the supported institution in the account transaction envelope is valid
+    When the ingest account transactions endpoint is invoked
+    Then the correct accepted response is returned from the ingest transactions endpoint
+    And the account transactions are published to the queue with the correct account transaction
